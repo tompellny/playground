f84318644bb098a65cdc15c367e7410ba5f325b1
Tom Pellny <thomas@pellny.com>
2025-10-24 01:12:51 +0200

    testing github push

--- home.py
+++ home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+st.title("Playground for Streamlit")
+
 option = st.selectbox(
     "Who helped you setting this up?",
     ("ChatGPT via Atlas", "Tim", "My Cat"),
